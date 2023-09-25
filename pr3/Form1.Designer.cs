623e83d422f5dffc1c6d833bb25fe32ecc54236d
YoloFmaster <yolofmaste@gmail.com>
2023-09-25 11:34:55 +0300

    Добавьте файлы проекта.

---
+++ pr3/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace pr3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.grBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.grBox2 = new System.Windows.Forms.GroupBox();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnP = new System.Windows.Forms.Button();
+            this.grBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btn939 = new System.Windows.Forms.Button();
+            this.btn839 = new System.Windows.Forms.Button();
+            this.btnStandart = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ffdsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.grBox2.SuspendLayout();
+            this.grBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grBox1
+            // 
+            this.grBox1.BackColor = System.Drawing.Color.Orange;
+            this.grBox1.Controls.Add(this.pictureBox5);
+            this.grBox1.Controls.Add(this.pictureBox2);
+            this.grBox1.Controls.Add(this.pictureBox4);
+            this.grBox1.Controls.Add(this.pictureBox1);
+            this.grBox1.Controls.Add(this.pictureBox3);
+            this.grBox1.Location = new System.Drawing.Point(31, 62);
+            this.grBox1.Name = "grBox1";
+            this.grBox1.Size = new System.Drawing.Size(317, 397);
+            this.grBox1.TabIndex = 0;
+            this.grBox1.TabStop = false;
+            this.grBox1.Text = "Расположение формы";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(117, 171);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(76, 80);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(6, 312);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(77, 79);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(225, 22);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(77, 79);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 79);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(225, 312);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(77, 79);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // grBox2
+            // 
+            this.grBox2.BackColor = System.Drawing.Color.Orange;
+            this.grBox2.Controls.Add(this.btnC);
+            this.grBox2.Controls.Add(this.btnB);
+            this.grBox2.Controls.Add(this.btnP);
+            this.grBox2.Location = new System.Drawing.Point(407, 62);
+            this.grBox2.Name = "grBox2";
+            this.grBox2.Size = new System.Drawing.Size(269, 140);
+            this.grBox2.TabIndex = 1;
+            this.grBox2.TabStop = false;
+            this.grBox2.Text = "Размер формы";
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(9, 96);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(251, 32);
+            this.btnC.TabIndex = 2;
+            this.btnC.Text = "Свернуть";
+            this.btnC.UseVisualStyleBackColor = true;
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(9, 58);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(251, 32);
+            this.btnB.TabIndex = 1;
+            this.btnB.Text = "Восстановить";
+            this.btnB.UseVisualStyleBackColor = true;
+            // 
+            // btnP
+            // 
+            this.btnP.Location = new System.Drawing.Point(9, 22);
+            this.btnP.Name = "btnP";
+            this.btnP.Size = new System.Drawing.Size(251, 32);
+            this.btnP.TabIndex = 0;
+            this.btnP.Text = "Развернуть";
+            this.btnP.UseVisualStyleBackColor = true;
+            this.btnP.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // grBox3
+            // 
+            this.grBox3.BackColor = System.Drawing.Color.Orange;
+            this.grBox3.Controls.Add(this.button4);
+            this.grBox3.Controls.Add(this.button2);
+            this.grBox3.Controls.Add(this.button1);
+            this.grBox3.Controls.Add(this.button3);
+            this.grBox3.Location = new System.Drawing.Point(760, 62);
+            this.grBox3.Name = "grBox3";
+            this.grBox3.Size = new System.Drawing.Size(264, 397);
+            this.grBox3.TabIndex = 1;
+            this.grBox3.TabStop = false;
+            this.grBox3.Text = "Информация о экране";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 327);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(251, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(251, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Orange;
+            this.groupBox4.Controls.Add(this.btnPlus);
+            this.groupBox4.Controls.Add(this.btn939);
+            this.groupBox4.Controls.Add(this.btn839);
+            this.groupBox4.Controls.Add(this.btnStandart);
+            this.groupBox4.Controls.Add(this.btnMinus);
+            this.groupBox4.Location = new System.Drawing.Point(407, 264);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(269, 195);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Размер формы";
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox4_Enter);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(179, 146);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 43);
+            this.btnPlus.TabIndex = 6;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            // 
+            // btn939
+            // 
+            this.btn939.Location = new System.Drawing.Point(3, 93);
+            this.btn939.Name = "btn939";
+            this.btn939.Size = new System.Drawing.Size(251, 32);
+            this.btn939.TabIndex = 5;
+            this.btn939.Text = "939 X 564";
+            this.btn939.UseVisualStyleBackColor = true;
+            // 
+            // btn839
+            // 
+            this.btn839.Location = new System.Drawing.Point(3, 55);
+            this.btn839.Name = "btn839";
+            this.btn839.Size = new System.Drawing.Size(251, 32);
+            this.btn839.TabIndex = 4;
+            this.btn839.Text = "839 X 464";
+            this.btn839.UseVisualStyleBackColor = true;
+            // 
+            // btnStandart
+            // 
+            this.btnStandart.Location = new System.Drawing.Point(3, 17);
+            this.btnStandart.Name = "btnStandart";
+            this.btnStandart.Size = new System.Drawing.Size(251, 32);
+            this.btnStandart.TabIndex = 3;
+            this.btnStandart.Text = "Стандартный";
+            this.btnStandart.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(9, 146);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 43);
+            this.btnMinus.TabIndex = 1;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ffdsToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(95, 26);
+            // 
+            // ffdsToolStripMenuItem
+            // 
+            this.ffdsToolStripMenuItem.Name = "ffdsToolStripMenuItem";
+            this.ffdsToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.ffdsToolStripMenuItem.Text = "ffds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 552);
+            this.Controls.Add(this.grBox2);
+            this.Controls.Add(this.grBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.grBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.grBox2.ResumeLayout(false);
+            this.grBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox grBox1;
+        private GroupBox grBox2;
+        private GroupBox grBox3;
+        private GroupBox groupBox4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private Button btnC;
+        private Button btnB;
+        private Button btnP;
+        private Button btnPlus;
+        private Button btn939;
+        private Button btn839;
+        private Button btnStandart;
+        private Button btnMinus;
+        private ContextMenuStrip contextMenuStrip1;
+        private ContextMenuStrip contextMenuStrip2;
+        private ToolStripMenuItem ffdsToolStripMenuItem;
+        private Button button4;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+    }
+}
---
+++ pr3/Form1.cs
@@ -0,0 +1,35 @@
+namespace pr3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
